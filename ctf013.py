33b8c1993bdea62f6b3f2635a73771205f34d3c9
thomas_ li <thomas_li@aliyun.com>
2018-08-04 14:26:52 +0800

    20180804 writeup

--- ctf013.py
+++ ctf013.py
@@ -1,11 +1,13 @@
+# coding=utf8
+
 import base64
 import re
 
-enstr = '''636A56355279427363446C4A49454A7154534230526D684356445A31614342354E326C4B4946467A5769426961453067'''
+enstr = '''666c61677b616537333538376261353662616566357d'''
 
 enstr2 = ""
 
-h2v = lambda a: int(ord(a) - ord('A') + 10) if (a >= 'A' and a <= 'F') else int(ord(a) - ord('0'))
+h2v = lambda a: int(ord(a) - ord('a') + 10) if (a >= 'a' and a <= 'f') else int(ord(a) - ord('0'))
 
 for p in range(int(len(enstr)/2)):
 
@@ -19,4 +21,4 @@ for p in range(int(len(enstr)/2)):
 
 print(enstr2)   
 print(len(enstr2)) 
-print(base64.b64decode(enstr2))
+print(base64.b64decode(enstr))
