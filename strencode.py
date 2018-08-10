d1130c17e6ee8cc86c02549dc9d695b869bf6132
thomas_ li <thomas_li@aliyun.com>
2018-08-10 17:32:31 +0800

    20180810 writeup

--- strencode.py
+++ strencode.py
@@ -18,11 +18,28 @@ def cp65001(name):
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
+
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+
 #sys.stderr = codecs.getwriter('utf8')(sys.stderr)
 #import codecs
 codecs.register(lambda name: codecs.lookup('utf-8') if name == 'cp65001' else None)
 
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+import os
+os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
+
+class UTF8StreamWriter:
+    def __init__(self, writer):
+        self.writer = writer
+    
+    def write(self, s):
+        #self.writer.write(s.encode('utf-8'))
+        os.write(self.writer.fileno(), s.encode('utf-8'))
+        
+sys.stdout = UTF8StreamWriter(sys.stdout)
 
 checkstr = '密码不正确'
 utf8=checkstr.encode(encoding='utf8')
@@ -37,4 +54,7 @@ for i in checkstr:
 for i in utf8.decode(encoding='utf8'):
     print ord(i)    
 
-#print utf8
+print checkstr
+
+#os.write(sys.stdout.fileno(), checkstr.encode('utf-8'))
+#os.write(sys.stdout.fileno(), checkstr)
